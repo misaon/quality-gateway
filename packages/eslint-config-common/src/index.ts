5a9c929161c8b3d84d802957cfb00f627c132c28
Ondřej Misák <email@ondrejmisak.cz>
2026-06-25 15:25:51 +0200

    feat(common): ignore caches and minified files

--- packages/eslint-config-common/src/index.ts
+++ packages/eslint-config-common/src/index.ts
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 /** Shared base config extended by every `@misaon/eslint-config-*` package. */
 export const common = defineConfig([
-  globalIgnores(['**/dist/**', '**/coverage/**', '**/node_modules/**']),
+  globalIgnores(['**/dist/**', '**/coverage/**', '**/node_modules/**', '**/.cache/**', '**/*.min.js']),
   // TODO: shared base rules
 ])
 
