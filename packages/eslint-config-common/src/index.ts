189ea9b95c3486c8b0c4e3cdae26c5ef4ba55416
Ondřej Misák <email@ondrejmisak.cz>
2026-06-25 16:44:44 +0200

    refactor(repo): remove comments from source and config files

--- packages/eslint-config-common/src/index.ts
+++ packages/eslint-config-common/src/index.ts
@@ -1,6 +1,5 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 
-/** Shared base config extended by every `@misaon/eslint-config-*` package. */
 export const common = defineConfig([
   globalIgnores([
     '**/dist/**',
@@ -9,7 +8,6 @@ export const common = defineConfig([
     '**/.cache/**',
     '**/*.min.js',
   ]),
-  // TODO: shared base rules
 ])
 
 export default common
--- packages/eslint-config-react/src/index.ts
+++ packages/eslint-config-react/src/index.ts
@@ -1,12 +1,10 @@
 import common from '@misaon/eslint-config-common'
 import { defineConfig } from 'eslint/config'
 
-/** React config. Base for `@misaon/eslint-config-next`, which extends it. */
 export const react = defineConfig([
   {
     name: '@misaon/react',
     extends: [common],
-    // TODO: React-specific plugins & rules
   },
 ])
 
